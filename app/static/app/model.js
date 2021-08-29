3c96343b729b5b54faa29637f2ef5ba2afa94fec
Anton Rösler <anton.roesler@stud.fra-uas.de>
2021-08-29 14:05:57 -0700

    Added user settings object
    Added an json object that is sent from the client to the server in the body of a POST request every time the user presses the continue button. In the object all the setting that the user has applied are sent. E.G, model=Scrum, junior-staff=5...

--- app/static/app/model.js
+++ app/static/app/model.js
@@ -1,29 +1,73 @@
 let x = new Vue({
-        el: "#vue",
-        delimiters: ['[[', ']]'],
-        data:{
-            blocks:
-          [
-              {
-                  header: "Story",
-                  text: "You are the Engineer!"
-              },
-              {
-                  header: "Time Line",
-                  text: "You are at the throttle"
-              }
-          ],
-            tasks: 0,
-            continue_text: "Continue"
-         }
-      });
-
+    el: "#vue",
+    delimiters: ['[[', ']]'],
+    data: {
+        blocks:
+            [
+                {
+                    header: "Story",
+                    text: "You are the Engineer!"
+                },
+                {
+                    header: "Time Line",
+                    text: "You are at the throttle"
+                }
+            ],
+        tasks: 0,
+        continue_text: "Continue"
+    }
+});
 
 
 /* Load Continue */
-let COUNTER = 0
-async function cont(){
-    const response = await fetch('continue?counter=' + COUNTER);
+let COUNTER = 0 // ToDo: Count on server side by having a flag at the current decision.
+
+function readButton(elementId) {
+    for (const button of document.getElementById(elementId).children){
+        console.log(button.classList)
+        if (button.classList.contains('active-button')){
+            return button.innerText;
+        }
+    }
+    return null;
+}
+
+
+function countStaff(staffType) {
+    return document.getElementById("staff-number-"+staffType).innerText.length;
+}
+
+/**
+ * Reads the current setting that the user as applied.
+ * (Status of all buttons etc.)
+ * @returns {*}
+ */
+function getSettings() {
+
+    return {
+        'model': readButton('model-picker-container'),
+        'lifecycle': readButton('lifecycle-picker-container'),
+        'staff': {
+            'junior': countStaff('junior'),
+            'senior': countStaff('senior'),
+            'expert': countStaff('expert'),
+            'consultant': countStaff('consultant')
+
+        }
+    };
+}
+
+async function cont() {
+    const response = await fetch('continue/?counter=' + COUNTER,
+        {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(getSettings())
+        }
+    );
     const data = await response.json();
     x._data.blocks = data.blocks;
     x._data.tasks = data.tasks;
--- app/views.py
+++ app/views.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+
 from app.src.domain.decision_tree import Scenario, Decision
 from mongo_models import ScenarioMongoModel
 
@@ -15,7 +17,10 @@ def index(request):
     return render(request, "app/index.html")
 
 
+@csrf_exempt
 def click_continue(request):
+    if request.method == 'POST':
+        print(request.body)
     counter = int(request.GET.get("counter"))
     model = ScenarioMongoModel()
     s = model.get(ObjectId("612baed001cb8bc5e9cb2cb8"))
