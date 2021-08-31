eceffd9f09da9ee75efcb76a47173d2c99929d69
Anton Rösler <anton.roesler@stud.fra-uas.de>
2021-08-30 17:37:05 -0700

    added first ui to create a scenario

--- app/static/app/instructor/add_scenario.js
+++ app/static/app/instructor/add_scenario.js
@@ -1,23 +1,47 @@
-
-
 const input = document.getElementById('name-input');
 const btn = document.getElementById('add-sc-btn');
 
-function checkValidity() {
-  if (hasWhiteSpace(input.value)){
-    input.classList.add("invalid")
-    btn.disabled = true;
-  } else {
-    input.classList.remove("invalid")
-    btn.disabled = false;
-  }
-}
-
 input.addEventListener('input', checkValidity)
 
+//btn.addEventListener('click', openDialog)
 
-
-
-function hasWhiteSpace(s) {
-  return s.indexOf(' ') >= 0;
+/**
+ * Checks if the input field is valid and adds/removes the css class for invalid fields.
+ */
+function checkValidity() {
+    if (hasWhiteSpace(input.value)) {
+        input.classList.add("invalid")
+        document.getElementById('add-sc-btn').disabled = true;
+        console.log(document.getElementById('add-sc-btn').disabled)
+    } else {
+        input.classList.remove("invalid");
+        document.getElementById('add-sc-btn').disabled = false;
+        console.log(btn.disabled)
+    }
 }
+
+/**
+ * Checks if the given String s contains a whitespace.
+ * @param s
+ * @returns {boolean}
+ */
+function hasWhiteSpace(s) {
+    return s.indexOf(' ') >= 0;
+}
+
+function openDialog() {
+    this.$root.$emit("bv::show::modal", "your-modal-id");
+}
+
+// register modal component
+Vue.component("modal", {
+    template: "#modal-template"
+});
+
+// start app
+new Vue({
+    el: "#app",
+    data: {
+        showModal: false,
+    }
+});
