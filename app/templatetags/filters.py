172e791fff1b41c3f44256c7507ce97f4e14f537
Anton Rösler <anton.roesler@stud.fra-uas.de>
2021-10-20 19:47:24 +0200

    Simple Styling for review page

--- app/templatetags/filters.py
+++ app/templatetags/filters.py
@@ -5,3 +5,9 @@ from django.template.defaultfilters import register
 def dict_key(d, k):
     """Returns the given key from a dictionary."""
     return d.get(k)
+
+
+@register.filter(name='round')
+def _round(f):
+    """Returns the float f rounded to two decimal places and converted to a percentage String."""
+    return str(int(round(f, 2)*100))
