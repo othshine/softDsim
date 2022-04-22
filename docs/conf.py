33b65bd6e34250a1582ca8d541ab612075e9f17d
Anton Rösler <anton.roesler98@gmx.de>
2022-04-22 20:26:33 +0200

    Update extensions

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,7 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'myst_parser'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -61,5 +62,3 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-# Markdown support
-extensions = ['myst_parser']
