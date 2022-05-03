715cb3a5afe2db6ec85001aca32714e0cd0d8e4e
Leonard Husske <leonard.husske@t-online.de>
2022-05-03 10:19:04 +0200

    task: #92 Initialize chakra ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,23 @@
-import React from "react";
-import {BrowserRouter} from "react-router-dom";
-import {Route, Routes} from "react-router";
+import React, {useEffect} from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Landing from "./pages/Landing";
 
 function App() {
+
+    useEffect(() => {
+        document.body.style.backgroundColor = "#EDF2F7";
+    }, []);
+
     return (
+        <>
+        <Navbar/>
         <BrowserRouter>
-            <Navbar/>
             <Routes>
                 <Route path="/" element={<Landing/>}/>
             </Routes>
         </BrowserRouter>
+        </>
     );
 }
 
--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,4 +1,4 @@
-import { Heading } from "@chakra-ui/react"
+import {Heading} from "@chakra-ui/react"
 import React from "react";
 
 const Landing = () => {
