9239f3b675cb4e6204b615eb6dcc0123c64085e0
Leonard Husske <leonard.husske@t-online.de>
2022-05-03 22:47:49 +0200

    feature: #102 Add scenario overview page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import React, {useEffect} from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Landing from "./pages/Landing";
+import ScenarioOverview from "./pages/ScenarioOverview";
+import Simulation from "./pages/Simulation";
 
 function App() {
 
@@ -11,10 +13,12 @@ function App() {
 
     return (
         <>
-        <Navbar/>
         <BrowserRouter>
+            <Navbar/>
             <Routes>
                 <Route path="/" element={<Landing/>}/>
+                <Route path="/scenarios" element={<ScenarioOverview/>}/>
+                <Route path="/scenarios/:scn_id" element={<Simulation/>}/>
             </Routes>
         </BrowserRouter>
         </>
