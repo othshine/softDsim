3407e1c3fe23b0f6bd040053c1b6205b4f4c66e0
nicki15 <nicolas93.wehner@googlemail.com>
2022-05-06 11:33:47 +0200

    task:#103 New Footer Style

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,16 +1,27 @@
-import { Flex, HStack, Button } from "@chakra-ui/react"
+import { Flex, HStack, Button, Text } from "@chakra-ui/react"
 import React from "react";
 
 const Footer = () => {
 
     return (
         <>
-            <Flex>
+            <Flex justify="center">
                 <HStack
-                    w="100%"
-                    justifyContent="center"
-                    gap={14}
-                    p={4}
+                    w="45%"
+                    justifyContent="start"
+                    align="center"
+                    gap={1}
+                    p={5}
+                >
+                    <Text textalign="center" textColor="gray.500" fontWeight="bold">
+                        © 2022 Simplify
+                    </Text>
+                </HStack>
+                <HStack
+                    w="45%"
+                    justifyContent="end"
+                    gap={10}
+                    p={5}
                 >
                     <Button variant='link'>
                         Imprint
