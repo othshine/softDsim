39530bd3928625c1bea66c539316acbf04bd9b61
LucasRue <lucas.rue@gmx.de>
2022-05-10 15:47:05 +0200

    task: #104 Impressum page

---
+++ frontend/src/pages/Imprint.jsx
@@ -0,0 +1,14 @@
+import { Flex, Heading, Text } from "@chakra-ui/react"
+import React from "react";
+
+const Imprint = () => {
+
+    return (
+        <Flex flexDir="column" flexGrow={1}>
+            <Heading>Imprint</Heading>
+            <Text> ***Information Missing*** </Text>
+        </Flex>
+    )
+}
+
+export default Imprint;
