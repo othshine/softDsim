21812ed7774c46f09a14290684e5107edb8caf2b
Leonard Husske <leonard.husske@t-online.de>
2022-05-10 15:14:50 +0200

    refactor: remove unnecessary import and format file

--- frontend/src/pages/Help.jsx
+++ frontend/src/pages/Help.jsx
@@ -1,15 +1,16 @@
 import {
-    Heading, Accordion,
-    AccordionItem,
+    Accordion,
     AccordionButton,
-    AccordionPanel,
     AccordionIcon,
+    AccordionItem,
+    AccordionPanel,
     Box,
     Breadcrumb,
     BreadcrumbItem,
-    BreadcrumbLink, Container,
-    TableContainer, Flex,
-
+    BreadcrumbLink,
+    Container,
+    Flex,
+    Heading,
 } from "@chakra-ui/react"
 import React from "react";
 import {HiChevronRight} from "react-icons/hi";
@@ -20,126 +21,126 @@ const Help = () => {
         <>
             <Flex px={10} pt={2} flexDir="column" flexGrow={1}>
 
-            <Breadcrumb spacing='8px' separator={<HiChevronRight color='gray.500'/>}>
-                <BreadcrumbItem>
-                    <BreadcrumbLink href=''>Help</BreadcrumbLink>
-                </BreadcrumbItem>
-            </Breadcrumb>
-            <Heading>Help and FAQ</Heading>
-            <Box  h={5}></Box>
-            <Box backgroundColor="white" borderRadius="2xl" minH="60vh">
-                <Container maxW='4xl' pt={10}>
-                    
+                <Breadcrumb spacing='8px' separator={<HiChevronRight color='gray.500'/>}>
+                    <BreadcrumbItem>
+                        <BreadcrumbLink href=''>Help</BreadcrumbLink>
+                    </BreadcrumbItem>
+                </Breadcrumb>
+                <Heading>Help and FAQ</Heading>
+                <Box h={5}></Box>
+                <Box backgroundColor="white" borderRadius="2xl" minH="60vh">
+                    <Container maxW='4xl' pt={10}>
+
                         <Accordion defaultIndex={[0]} allowMultiple='true' allowToggle='true'>
-  <AccordionItem>
-    <h2>
-      <AccordionButton _expanded={{bg: 'blue', color: 'white'}}>
-        <Box flex='1' textAlign='left'>
-          Einleitung
-        </Box >
-        <AccordionIcon />
-      </AccordionButton>
-    </h2>
-    <AccordionPanel pb={4}>
-    
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.
-      
-    </AccordionPanel>
-  </AccordionItem>
+                            <AccordionItem>
+                                <h2>
+                                    <AccordionButton _expanded={{bg: 'blue', color: 'white'}}>
+                                        <Box flex='1' textAlign='left'>
+                                            Einleitung
+                                        </Box>
+                                        <AccordionIcon/>
+                                    </AccordionButton>
+                                </h2>
+                                <AccordionPanel pb={4}>
 
-  <AccordionItem>
-    <h2>
-      <AccordionButton _expanded={{bg: 'blue', color: 'white'}}>
-        <Box flex='1' textAlign='left'>
-          Registration/Login
-        </Box>
-        <AccordionIcon />
-      </AccordionButton>
-    </h2>
-    <AccordionPanel pb={4}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.
-    </AccordionPanel>
-  </AccordionItem>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                    commodo consequat.
 
-  <AccordionItem>
-    <h2>
-      <AccordionButton _expanded={{bg: 'blue', color: 'white'}}>
-        <Box flex='1' textAlign='left'>
-          Scenarios
-        </Box>
-        <AccordionIcon />
-      </AccordionButton>
-    </h2>
-    <AccordionPanel pb={4}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.
-    </AccordionPanel>
-  </AccordionItem>
+                                </AccordionPanel>
+                            </AccordionItem>
 
-  <AccordionItem>
-    <h2>
-      <AccordionButton _expanded={{bg: 'blue', color: 'white'}}>
-        <Box flex='1' textAlign='left'>
-          Scenario Studio
-        </Box>
-        <AccordionIcon />
-      </AccordionButton>
-    </h2>
-    <AccordionPanel pb={4}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.
-    </AccordionPanel>
-  </AccordionItem>
-  <AccordionItem>
-    <h2>
-      <AccordionButton _expanded={{bg: 'blue', color: 'white'}}>
-        <Box flex='1' textAlign='left'>
-          Profile
-        </Box>
-        <AccordionIcon />
-      </AccordionButton>
-    </h2>
-    <AccordionPanel pb={4}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.
-    </AccordionPanel>
-  </AccordionItem>
-  <AccordionItem>
-    <h2>
-      <AccordionButton _expanded={{bg: 'blue', color: 'white'}}>
-        <Box flex='1' textAlign='left'>
-          Additional
-        </Box>
-        <AccordionIcon />
-      </AccordionButton>
-    </h2>
-    <AccordionPanel pb={4}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.
-    </AccordionPanel>
-    
-  </AccordionItem>
-  
-  
-</Accordion>
-                    
-                </Container>
-            </Box>
-        </Flex>
+                            <AccordionItem>
+                                <h2>
+                                    <AccordionButton _expanded={{bg: 'blue', color: 'white'}}>
+                                        <Box flex='1' textAlign='left'>
+                                            Registration/Login
+                                        </Box>
+                                        <AccordionIcon/>
+                                    </AccordionButton>
+                                </h2>
+                                <AccordionPanel pb={4}>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                    commodo consequat.
+                                </AccordionPanel>
+                            </AccordionItem>
+
+                            <AccordionItem>
+                                <h2>
+                                    <AccordionButton _expanded={{bg: 'blue', color: 'white'}}>
+                                        <Box flex='1' textAlign='left'>
+                                            Scenarios
+                                        </Box>
+                                        <AccordionIcon/>
+                                    </AccordionButton>
+                                </h2>
+                                <AccordionPanel pb={4}>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                    commodo consequat.
+                                </AccordionPanel>
+                            </AccordionItem>
+
+                            <AccordionItem>
+                                <h2>
+                                    <AccordionButton _expanded={{bg: 'blue', color: 'white'}}>
+                                        <Box flex='1' textAlign='left'>
+                                            Scenario Studio
+                                        </Box>
+                                        <AccordionIcon/>
+                                    </AccordionButton>
+                                </h2>
+                                <AccordionPanel pb={4}>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                    commodo consequat.
+                                </AccordionPanel>
+                            </AccordionItem>
+                            <AccordionItem>
+                                <h2>
+                                    <AccordionButton _expanded={{bg: 'blue', color: 'white'}}>
+                                        <Box flex='1' textAlign='left'>
+                                            Profile
+                                        </Box>
+                                        <AccordionIcon/>
+                                    </AccordionButton>
+                                </h2>
+                                <AccordionPanel pb={4}>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                    commodo consequat.
+                                </AccordionPanel>
+                            </AccordionItem>
+                            <AccordionItem>
+                                <h2>
+                                    <AccordionButton _expanded={{bg: 'blue', color: 'white'}}>
+                                        <Box flex='1' textAlign='left'>
+                                            Additional
+                                        </Box>
+                                        <AccordionIcon/>
+                                    </AccordionButton>
+                                </h2>
+                                <AccordionPanel pb={4}>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                    commodo consequat.
+                                </AccordionPanel>
+
+                            </AccordionItem>
+
+
+                        </Accordion>
+
+                    </Container>
+                </Box>
+            </Flex>
         </>
     )
 }
