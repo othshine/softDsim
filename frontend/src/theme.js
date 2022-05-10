93e97aa33f09da6538605000341d0b99caf08828
LucasRue <lucas.rue@gmx.de>
2022-05-10 13:49:28 +0200

    task: #117 Create Color Class

---
+++ frontend/src/theme.js
@@ -0,0 +1,24 @@
+// theme.js
+export default {
+    colors: {
+        transparent: 'transparent',
+        black: '#000',
+        white: '#fff',
+        gray: {
+            100: '#EDF2F7',
+            400: '#A0AEC0',
+        },
+        blue: {
+            50: '#EBF8FF',
+            100: '#BEE3F8',
+            200: '#90CDF4',
+            300: '#63B3ED',
+            400: '#4299E1',
+            500: '#3182CE',
+            600: '#2B6CB0',
+            700: '#2C5282',
+            800: '#2A4365',
+            900: '#171923',
+        },
+    },
+}
