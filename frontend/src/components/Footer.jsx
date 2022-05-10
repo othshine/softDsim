42c4ae5a072fba0232acc5b9fd63039fd7730c58
nicki15 <nicolas93.wehner@googlemail.com>
2022-05-10 15:41:12 +0200

    task: #105 GDPR page

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import { Flex, HStack, Button, Text } from "@chakra-ui/react"
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
 
@@ -26,7 +27,7 @@ const Footer = () => {
                     <Button variant='link'>
                         Imprint
                     </Button>
-                    <Button variant='link'>
+                    <Button variant='link' as={Link} to="/GDPR">
                         GDPR
                     </Button>
                 </HStack>
