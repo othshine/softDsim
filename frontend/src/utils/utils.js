69b5de34a49dc9dafea4e4b9d8c8648121ddb4c4
Leonard Husske <leonard.husske@t-online.de>
2022-05-11 23:33:16 +0200

    feature: #180 add global role enum

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,7 +1,12 @@
-function getCookie(name) {
+export function getCookie(name) {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) return parts.pop().split(';').shift();
 }
 
-export default getCookie;
+export const role = {
+    ADMIN: "admin",
+    STAFF: "staff",
+    CREATOR: "creator",
+    STUDENT: "student"
+}
