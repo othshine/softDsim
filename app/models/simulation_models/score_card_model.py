5476348658e23ab843e98ba3a78aa3b4430c734a
philip <philipmertz@live.de>
2022-05-13 11:30:26 +0200

    bug: #137 changed ScoreCard model

--- app/models/simulation_models/score_card_model.py
+++ app/models/simulation_models/score_card_model.py
@@ -5,10 +5,14 @@ from app.models.simulation_models.simulation_model import Simulation
 
 class ScoreCard(models.Model):
     id = models.AutoField(primary_key=True)
-    easy = models.PositiveIntegerField()
-    medium = models.PositiveIntegerField()
-    hard = models.PositiveIntegerField()
+    budget_limit = models.PositiveIntegerField(default=100)
+    time_limit = models.PositiveIntegerField(default=100)
+    quality_limit = models.PositiveIntegerField(default=100)
+    budget_p = models.FloatField(default=1.0)
+    time_p = models.FloatField(default=1.0)
+    quality_k = models.PositiveIntegerField(default=8)
 
     simulation = models.OneToOneField(
         Simulation, on_delete=models.CASCADE, related_name="score_card"
     )
+
