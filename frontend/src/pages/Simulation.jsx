d650f90af629ccdc4605efade419d1228f57b9ea
LucasRue <lucas.rue@gmx.de>
2022-05-13 09:03:33 +0200

    task: 171 Create Input Elements for Dashboard

--- frontend/src/pages/Simulation.jsx
+++ frontend/src/pages/Simulation.jsx
@@ -1,4 +1,4 @@
-import { Heading, Container, Grid, GridItem, Flex, Spacer, Box } from "@chakra-ui/react"
+import { Heading, Container, Grid, GridItem, Flex, Spacer, Box, Button, Radio, RadioGroup, Stack } from "@chakra-ui/react"
 import React from "react";
 
 const Simulation = () => {
@@ -26,7 +26,7 @@ const Simulation = () => {
                     </Box>
                     <Spacer />
                     <Box
-                        p='5'
+                        p='3'
                         w='38%'
                         boxShadow='md'
                         rounded='md'
@@ -37,22 +37,90 @@ const Simulation = () => {
                             <b>Actions</b>
                         </p>
                         <Grid
-
-                            templateRows='repeat(10, 1fr)'
+                            templateRows='repeat(4, 1fr)'
                             templateColumns='repeat(3, 1fr)'
                             gap={4}
                             p='5'
                             justify="flex-end"
                         >
-                            <GridItem colSpan={3} _hover={{ boxShadow: '2xl' }} boxShadow='md' rounded='md' bg='gray.100'> Choice 1 </GridItem>
-                            <GridItem colSpan={3} _hover={{ boxShadow: '2xl' }} boxShadow='md' rounded='md' bg='gray.100'> Choice 2 </GridItem>
-                            <GridItem colSpan={3} _hover={{ boxShadow: '2xl' }} boxShadow='md' rounded='md' bg='gray.100'> Choice 3 </GridItem>
-                            <GridItem colSpan={3} _hover={{ boxShadow: '2xl' }} boxShadow='md' rounded='md' bg='gray.100'> Choice 4 </GridItem>
-                            <GridItem colSpan={3} _hover={{ boxShadow: '2xl' }} boxShadow='md' rounded='md' bg='gray.100'> Choice 5 </GridItem>
-                            <GridItem colSpan={3} _hover={{ boxShadow: '2xl' }} boxShadow='md' rounded='md' bg='gray.100'> Choice 6 </GridItem>
-                            <GridItem colSpan={3} _hover={{ boxShadow: '2xl' }} boxShadow='md' rounded='md' bg='gray.100'> Choice 7 </GridItem>
+                            <GridItem colSpan={3} _hover={{ boxShadow: '2xl' }} boxShadow='md' rounded='md' bg='gray.100' m='2'>
+                                <Grid>
+                                    <GridItem colSpan={3} rounded='md' bg='gray.100' p='2'> <b>Hier steht eine Frage</b> </GridItem>
+                                    <RadioGroup defaultValue='1'>
+                                        <Stack spacing={4} direction='row' p='2' justify='flex-end'>
+                                            <Radio value='1'>
+                                                Answer 1
+                                            </Radio>
+                                            <Radio value='2'>
+                                                Answer 2
+                                            </Radio>
+                                            <Radio value='3'>
+                                                Answer 3
+                                            </Radio>
+                                        </Stack>
+                                    </RadioGroup>
+                                </Grid>
+                            </GridItem>
+                            <GridItem colSpan={3} _hover={{ boxShadow: '2xl' }} boxShadow='md' rounded='md' bg='gray.100' m='2'>
+                                <Grid>
+                                    <GridItem colSpan={3} rounded='md' bg='gray.100' p='2'> <b>Hier steht eine Frage</b> </GridItem>
+                                    <RadioGroup defaultValue='1'>
+                                        <Stack spacing={4} direction='row' p='2' justify='flex-end'>
+                                            <Radio value='1'>
+                                                Answer 1
+                                            </Radio>
+                                            <Radio value='2'>
+                                                Answer 2
+                                            </Radio>
+                                            <Radio value='3'>
+                                                Answer 3
+                                            </Radio>
+                                        </Stack>
+                                    </RadioGroup>
+                                </Grid>
+                            </GridItem>
+                            <GridItem colSpan={3} _hover={{ boxShadow: '2xl' }} boxShadow='md' rounded='md' bg='gray.100' m='2'>
+                                <Grid>
+                                    <GridItem colSpan={3} rounded='md' bg='gray.100' p='2'> <b>Hier steht eine Frage</b> </GridItem>
+                                    <RadioGroup defaultValue='1'>
+                                        <Stack spacing={4} direction='row' p='2' justify='flex-end'>
+                                            <Radio value='1'>
+                                                Answer 1
+                                            </Radio>
+                                            <Radio value='2'>
+                                                Answer 2
+                                            </Radio>
+                                            <Radio value='3'>
+                                                Answer 3
+                                            </Radio>
+                                        </Stack>
+                                    </RadioGroup>
+                                </Grid>
+                            </GridItem>
+                            <GridItem colSpan={3} _hover={{ boxShadow: '2xl' }} boxShadow='md' rounded='md' bg='gray.100' m='2'>
+                                <Grid>
+                                    <GridItem colSpan={3} rounded='md' bg='gray.100' p='2'> <b>Hier steht eine Frage</b> </GridItem>
+                                    <RadioGroup defaultValue='1'>
+                                        <Stack spacing={4} direction='row' p='2' justify='flex-end'>
+                                            <Radio value='1'>
+                                                Answer 1
+                                            </Radio>
+                                            <Radio value='2'>
+                                                Answer 2
+                                            </Radio>
+                                            <Radio value='3'>
+                                                Answer 3
+                                            </Radio>
+                                        </Stack>
+                                    </RadioGroup>
+                                </Grid>
+                            </GridItem>
+
                             <GridItem colSpan={2} />
-                            <GridItem colSpan={1} _hover={{ boxShadow: '2xl' }} boxShadow='md' rounded='md' bg='blue.500' color='white' fontWeight='bold'> Next Week </GridItem>
+                            <GridItem colSpan={1}  >
+                                <Button colorScheme='blue' size='lg'>
+                                    Next Week
+                                </Button> </GridItem>
                         </Grid>
                     </Box>
                 </Flex >
